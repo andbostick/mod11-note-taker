0de601228db940f2af51996072a852962aa87ccc
Andrew <andbostick@gmail.com>
2023-08-17 19:02:38 -0400

    function to save a note to the db

--- server.js
+++ server.js
@@ -6,8 +6,30 @@ const notes = require("./db/db.json");
 
 const app = express();
 
+function createNewNote(note, arr) {
+    let count = 0;
+    const newNote = note;
+    if (!Array.isArray(arr))
+        arr = [];
+    
+    if (arr.length === 0)
+        arr.push(0);
+
+    note.id = count;
+    count++;
+
+    arr.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(arr, null, 2)
+    );
+    return newNote;
+}
+
+
 app.use(express.json());
 app.use(express.static("public"));
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
@@ -18,13 +40,17 @@ app.get('/notes', (req, res) => {
 });
 
 app.get('/api/notes', (req, res) => {
-    res.json(notes);
+    res.json(notes.slice(1));
 });
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+app.post('/api/notes', (req, res) => {
+    const newNote = createNewNote(req.body, notes);
+    res.json(newNote);
+});
 
 
 app.listen(PORT, () => {
