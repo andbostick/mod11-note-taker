87a7ee1f0b531ed88c80fb41153c0858ae275377
Andrew <andbostick@gmail.com>
2023-08-17 19:35:59 -0400

    trying to fix a server error

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const path = require("path");
 const fs = require("fs");
 const notes = require("./db/db.json");
