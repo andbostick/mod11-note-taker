4fa43bcf4dc725d0a55b49927990ebb5a979946e
Andrew <andbostick@gmail.com>
2023-08-17 18:50:02 -0400

    route to get api data

--- server.js
+++ server.js
@@ -17,9 +17,15 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
+
+
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
